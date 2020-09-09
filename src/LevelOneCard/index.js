d9ef35c741e2ac8ff29e5b421ca0aec5323c0b0c
Shannon Bonet <shannonbonet@berkeley.edu>
2020-09-08 20:37:08 -0700

    airtable

--- src/LevelOneCard/index.js
+++ src/LevelOneCard/index.js
@@ -2,12 +2,34 @@ import React from "react";
 import "./styles.css";
 import GameCards from "../GameCards/index.js";
 
-function LevelOneCard() {
-  return (
+class LevelOneCard extends React.Component{
+  constructor(props) { super(props); 
+      this.state = {qList: []}; 
+  }
+
+  componentDidMount () {
+    fetch('https://api.airtable.com/v0/appMu2ml24abXK2X4/PercepQues?api_key=API_KEY&view=Grid%20view') //returns promise // 
+    .then(res => res.json()) // returns promise that gives json // 
+    .then(res => res.records)
+    .then(res => {
+      let qmap = res.map(record => record.fields.Question)
+      console.log(qmap)
+      this.setState({ qList: qmap })
+    })
+    .catch(error => console.log(error))
+}
+
+  
+  
+  render () {
+    const questions = this.state.qList;
+    return (
     <div className="Level1Card">
-      <GameCards label="what was your first impression of me?" />
-    </div>
-  );
+      {questions 
+      ? <GameCards label = {questions[3]}></GameCards>
+      : <GameCards label = {"No questions"}></GameCards>}
+    </div>)
+  }
 }
 
 export default LevelOneCard;
