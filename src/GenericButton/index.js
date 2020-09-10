88ce3b784e8e5e1dd936a66c6fb53df2035fd1de
Kyle Hua <kyleqhua@gmail.com>
2020-09-10 15:33:25 -0700

    basic speed dating working

--- src/GenericButton/index.js
+++ src/GenericButton/index.js
@@ -4,7 +4,7 @@ import './styles.css';
 class GenericButton extends React.PureComponent {
     constructor(props) { super(props) }
     render() {
-        return (<button className='g-button'>{this.props.label}</button>);
+        return (<button className='g-button' onClick={this.props.changeQ}>{this.props.label} </button>);
     }
 }
 
--- src/LevelOneCard/index.js
+++ src/LevelOneCard/index.js
@@ -1,34 +1,43 @@
 import React from "react";
 import "./styles.css";
 import GameCards from "../GameCards/index.js";
+import GenericButton from '../GenericButton/index.js'
+class LevelOneCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      qList: [],
+      selectedQ: "Click the button for a new question"
+    };
 
-class LevelOneCard extends React.Component{
-  constructor(props) { super(props); 
-      this.state = {qList: []}; 
   }
 
-  componentDidMount () {
-    fetch('https://api.airtable.com/v0/appMu2ml24abXK2X4/PercepQues?api_key=API_KEY&view=Grid%20view') //returns promise // 
-    .then(res => res.json()) // returns promise that gives json // 
-    .then(res => res.records)
-    .then(res => {
-      let qmap = res.map(record => record.fields.Question)
-      console.log(qmap)
-      this.setState({ qList: qmap })
-    })
-    .catch(error => console.log(error))
-}
+  componentDidMount() {
+    //this is burner airtable account with burner keys.
+    fetch('https://api.airtable.com/v0/appzuxlfcCMsUs8Nb/Speed?api_key=keyIol9Z3RTY1PZr7&view=Grid%20view')
+      .then(res => res.json())
+      .then(res => res.records)
+      .then(res => {
+        let qmap = res.map(record => record.fields.Notes)
+        console.log(qmap)
+        this.setState({ qList: qmap })
+      })
+      .catch(error => console.log(error))
+  }
 
-  
-  
-  render () {
-    const questions = this.state.qList;
+  getRandomCard() {
+    let questions = this.state.qList;
+    let index = Math.floor(Math.random() * questions.length);
+    this.setState({ selectedQ: questions[index] });
+  }
+
+  render() {
+    let question = this.state.selectedQ
     return (
-    <div className="Level1Card">
-      {questions 
-      ? <GameCards label = {questions[3]}></GameCards>
-      : <GameCards label = {"No questions"}></GameCards>}
-    </div>)
+      <div className="Level1Card">
+        <GameCards label={question}></GameCards>
+        <GenericButton label='Press to Change Question' changeQ={this.getRandomCard.bind(this)} />
+      </div>)
   }
 }
 
